d0d353bab4878796aad7d1b4b8b920ea15fdf3e7
Stravinskaitė <stravinskaite.g@gmail.com>
2020-10-12 12:40:33 +0200

    some speed break

--- src/main/java/com/virtualpairprogrammers/tracker/messaging/MessageProcessor.java
+++ src/main/java/com/virtualpairprogrammers/tracker/messaging/MessageProcessor.java
@@ -33,6 +33,7 @@ public class MessageProcessor {
 				                          .withLat(new BigDecimal(incomingMessage.get("lat")))
 				                          .withLng(new BigDecimal(incomingMessage.get("long")))
 				                          .withTimestamp(convertedDatestamp)
+										  .withSpeed(47.5)
 				                          .build();
 				                          
 		data.updatePosition(newReport);
