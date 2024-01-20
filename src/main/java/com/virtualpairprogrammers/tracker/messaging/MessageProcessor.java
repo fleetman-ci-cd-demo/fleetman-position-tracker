309a5ecc24355e5e20551a3045dbfc3c5a6ab55f
Dick Chesterwood <richard@inceptiontraining.co.uk>
2024-01-20 19:46:58 +0000

    reinstating the 0 error

--- src/main/java/com/virtualpairprogrammers/tracker/messaging/MessageProcessor.java
+++ src/main/java/com/virtualpairprogrammers/tracker/messaging/MessageProcessor.java
@@ -34,7 +34,7 @@ public class MessageProcessor {
 				                          .withLat(new BigDecimal(incomingMessage.get("lat")))
 				                          .withLng(new BigDecimal(incomingMessage.get("long")))
 				                          .withTimestamp(convertedDatestamp)
-                                                          .withSpeed(new BiadskfjhaskjdfasdfgDecimal(0))
+                                                          .withSpeed(new BigDecimal(47.5))
 				                          .build();
 				                          
 		data.updatePosition(newReport);
